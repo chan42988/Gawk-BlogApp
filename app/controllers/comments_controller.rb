b3c5b56dda90028701b5cbf786a8d464f740792d
Christopher <chan42988@gmail.com>
2016-05-09 22:28:34 -0400

    comments added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
   def create
+  		current_user
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to post_path(@comment.post_id)
   end
 
   def update
@@ -7,4 +11,11 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def comment_params
+  	params.require(:comment).permit(:body, :user_id, :post_id)
+  end
+
 end
